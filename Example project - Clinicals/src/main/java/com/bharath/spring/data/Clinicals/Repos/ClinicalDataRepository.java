5562df245872520bf190b9b76eeb4135069ac309
tgoldboom <tgoldboom@googlemail.com>
2022-02-01 15:47:26 +0100

    Cleaned up project

--- Example project - Clinicals/src/main/java/com/bharath/spring/data/Clinicals/Repos/ClinicalDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bharath.spring.data.Clinicals.Repos;
-
-import com.bharath.spring.data.Clinicals.Entities.ClinicalData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClinicalDataRepository extends JpaRepository <ClinicalData, Integer> {
-}
--- Example project - Clinicals/src/main/java/com/bharath/spring/data/Clinicals/Repos/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bharath.spring.data.Clinicals.Repos;
-
-import com.bharath.spring.data.Clinicals.Entities.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository <Patient, Integer> {
-}
