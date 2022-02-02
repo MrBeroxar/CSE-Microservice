ee511fc3780154e6ac4a4603357d325d6847421f
timwhooks <timwhooks@tims-air>
2022-02-02 21:28:17 +0100

    Grpc api added

---
+++ ratingService/src/main/java/cse/project/persistence/PersistenceService.java
@@ -0,0 +1,13 @@
+package cse.project.persistence;
+
+public class PersistenceService {
+	
+	public void saveRating(long productId, long rating) {
+		System.out.print("penis");
+	}
+	public long getRating(long productId) {
+		System.out.print("yaaass");
+		return 12;
+	}
+	
+}
---
+++ ratingService/src/main/java/cse/project/ratingservice/RatingService.java
@@ -0,0 +1,37 @@
+package cse.project.ratingservice;
+
+import cse.project.grpc.ApiResponse;
+import cse.project.grpc.ProductRequest;
+import cse.project.grpc.RatingRequest;
+import cse.project.grpc.RatingResponse;
+import cse.project.grpc.RatingServiceGrpc.RatingServiceImplBase;
+import cse.project.persistence.PersistenceService;
+import io.grpc.stub.StreamObserver;
+
+public class RatingService extends RatingServiceImplBase{
+	
+	private PersistenceService persistenceService = new PersistenceService();
+	@Override
+	public void rateProduct(ProductRequest request, StreamObserver<ApiResponse> responseObserver) {
+		
+		persistenceService.saveRating(request.getProductId(), request.getRating());
+		ApiResponse.Builder response = ApiResponse.newBuilder();
+		
+		response.setResponseMessage("SUCCESS: Rating saved");
+		responseObserver.onNext(response.build());
+		responseObserver.onCompleted();
+	}
+
+	@Override
+	public void getProductRating(RatingRequest request, StreamObserver<RatingResponse> responseObserver) {
+		
+		RatingResponse.Builder response = RatingResponse.newBuilder();
+		
+		response.setRating(persistenceService.getRating(request.getProductId()));
+		response.setResponseMessage("SUCESS: Rating send");
+		responseObserver.onNext(response.build());
+		responseObserver.onCompleted();
+	}
+	
+	
+}
