c49975a2b41314446d212077505315a2f8172d43
timwhooks <timwhooks@tims-air.fritz.box>
2022-02-02 21:31:12 +0100

    updated

--- ratingService/src/main/java/cse/project/persistence/PersistenceService.java
+++ ratingService/src/main/java/cse/project/persistence/PersistenceService.java
@@ -3,10 +3,10 @@ package cse.project.persistence;
 public class PersistenceService {
 	
 	public void saveRating(long productId, long rating) {
-		System.out.print("penis");
+		
 	}
 	public long getRating(long productId) {
-		System.out.print("yaaass");
+		
 		return 12;
 	}
 	
