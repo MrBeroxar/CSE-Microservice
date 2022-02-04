19bcc99f718a7c322f2e2284a52d6f0ccf92bb14
tgoldboom <tgoldboom@googlemail.com>
2022-02-04 09:10:53 +0100

    Updated DB creation scripts

---
+++ MockDbData.sql
@@ -0,0 +1,15 @@
+use postgres;
+
+INSERT INTO product_ratings(product_id, rating) VALUES('0PUK6V6EV0', 5);
+INSERT INTO product_ratings(product_id, rating) VALUES('0PUK6V6EV0', 4);
+INSERT INTO product_ratings(product_id, rating) VALUES('0PUK6V6EV0', 3);
+INSERT INTO product_ratings(product_id, rating) VALUES('0PUK6V6EV0', 4);
+INSERT INTO product_ratings(product_id, rating) VALUES('0PUK6V6EV0', 5);
+INSERT INTO product_ratings(product_id, rating) VALUES('0PUK6V6EV0', 1);
+
+INSERT INTO shop_ratings(rating) VALUES(5);
+INSERT INTO shop_ratings(rating) VALUES(4);
+INSERT INTO shop_ratings(rating) VALUES(3);
+INSERT INTO shop_ratings(rating) VALUES(4);
+INSERT INTO shop_ratings(rating) VALUES(5);
+INSERT INTO shop_ratings(rating) VALUES(1);
